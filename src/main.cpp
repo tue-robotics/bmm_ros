fb63af7d984ea93db926b8cb25c06cd1c1fa0a1f
IasonTheodorou <iasonth95@gmail.com>
2025-09-06 11:23:13 +0200

    Added files to make it a catkin pkg and created skeleton of main and CMakeLists (still not working)

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "bayesian_gmm.h"
+#include "variational_gmm.h"
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <Eigen/Dense>
+
+GMMParams params;
+config_.value("psi0", params.psi0);
+config_.value("nu0", params.nu0);
+config_.value("alpha", params.alpha);
+config_.value("kappa0", params.kappa0);
+
+
+MAPGMM gmm(2, cluster.points, params); // 2 components: object + outliers
+gmm.fit(cluster.points, sensor_pose);
+// Get component assignments and inlier component
+std::vector<int> labels = gmm.get_labels();
+int inlier_component = gmm.get_inlier_component();
+// Filter points
+std::vector<geo::Vec3> filtered_points;
+for (size_t i = 0; i < labels.size(); i++) {
+    if (labels[i] == inlier_component) {
+        filtered_points.push_back(cluster.points[i]);
+    }
+}
+// Safety check
+if (filtered_points.size() > 10 && filtered_points.size() > 0.1 * cluster.points.size()) {
+    ROS_INFO("MAP-GMM filtering: kept %zu of %zu points (%.1f%%)",
+            filtered_points.size(), cluster.points.size(),
+            100.0 * filtered_points.size() / cluster.points.size());
+    cluster.points = filtered_points;
+} else {
+    ROS_WARN("MAP-GMM filtering: too few points kept, using original points");
+}
